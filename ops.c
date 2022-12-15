35bee9fda96c8d69da8145049e7faa2f6f59f64d
stanley <stanley6453@gmail.com>
2022-12-15 09:37:23 +0100

    implemented pop

--- ops.c
+++ ops.c
@@ -83,7 +83,7 @@ void pop(stack_t **stack, unsigned int line_number)
 {
 	stack_t *temp;
 
-	if (*stack == NULL)
+	if (*stack == NULL || stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		freeallandexit(*stack, gv.file, *gv.line, EXIT_FAILURE);
